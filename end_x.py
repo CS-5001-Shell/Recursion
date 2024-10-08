b6fd3be5d465dc9aa48e7766a98c81c7e9c6d9f1
Sami Rollins <srollins@gmail.com>
2024-10-07 19:10:06 -0700

    fix formatting

--- end_x.py
+++ end_x.py
@@ -9,7 +9,9 @@ endX("xhixhix") -> "hihixxx"
 """
 def end_x(word: str) -> str:
     """
-    A recursive Python function that takes as a parameter a string and recursively computes a new string where all the lowercase 'x' chars have been moved to the end of the string. The function should take only one parameter and not use any loops.
+    A recursive Python function that takes as a parameter a string and
+    recursively computes a new string where all the lowercase 'x' chars have
+    been moved to the end of the string. The function should take only one parameter and not use any loops.
 
     endX("xxre") -> "rexx"
     endX("xxhixx") -> "hixxxx"
